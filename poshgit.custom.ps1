5e6b4f5b4eba131e8d1dd6750cdf047175115957
Charles Roper <charles@roper.im>
2015-04-01 14:43:45 +0100

    Remove old prompt styling

--- poshgit.custom.ps1
+++ poshgit.custom.ps1
@@ -15,10 +15,6 @@ function global:prompt {
     # Reset color, which can be messed up by Enable-GitColors
     $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
 
-    # Improved prompt
-    # http://ss64.com/ps/syntax-prompt.html
-    # Write-Host('PS ' + ($pwd -split '\\')[0]+' '+$(($pwd -split '\\')[-1] -join '\')) -NoNewline
-
     write-host ([net.dns]::GetHostName()) -NoNewline -ForegroundColor Green
     write-host (' ' + $pwd.Path.Replace($env:Home, '~')) -NoNewline -ForegroundColor DarkCyan
     Write-VcsStatus
