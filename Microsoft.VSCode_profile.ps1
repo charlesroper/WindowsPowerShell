fd36d49ef154db4ad44f281306661bc23e5d5d42
Charles Roper <charles@roper.im>
2019-07-03 02:35:17 +0100

    Add oh-my-posh config

--- Microsoft.VSCode_profile.ps1
+++ Microsoft.VSCode_profile.ps1
@@ -1,3 +1,6 @@
 # Load my default PowerShell profile into the integrated VSCode terminal
 $docs = [System.Environment]::GetFolderPath("mydocuments")
 . (Join-Path -Path $docs -ChildPath 'WindowsPowerShell\Microsoft.PowerShell_profile.ps1')
+
+$ThemeSettings.GitSymbols.BranchSymbol = [char]::ConvertFromUtf32(0x03BB)
+$ThemeSettings.GitSymbols.BranchIdenticalStatusToSymbol = [char]::ConvertFromUtf32(0x2263)
